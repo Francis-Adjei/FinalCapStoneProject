ad2a7719d0ce805d58c6dd6a03394b0ab39cdcb1
Francis-Adjei <francis.adjei@amalitech.org>
2021-08-11 15:28:42 +0000

    FinalCapStoneProject

--- src/components/HomeComponent/Home.js
+++ src/components/HomeComponent/Home.js
@@ -25,7 +25,10 @@ const Home = (props) => {
                 <footer>
                     <h3>QUOTABLE</h3>
                     <p>Created by Francis Adjei</p>
-                    <span><a href="https://github.com/Francis-Adjei/FinalCapStoneProject"><i className="fa fa-github" aria-hidden="true"></i></a><a href="https://www.linkedin.com/in/adjei-francis-91a40016b/"><i className="fa fa-linkedin" aria-hidden="true"></i></a></span>
+                    <span>
+                        <a href="https://github.com/Francis-Adjei/FinalCapStoneProject"><i className="fa fa-github" aria-hidden="true"></i></a>
+                        <a href="https://www.linkedin.com/in/adjei-francis-91a40016b/"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
+                    </span>
                 </footer>
             </div>
         </section>
